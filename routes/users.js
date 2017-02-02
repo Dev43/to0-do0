1d390ff66ad4bee27ddb27a28f5bace56bd7c467
Dev43 <patrick.guay43@gmail.com>
2017-02-02 16:51:58 -0500

    finished implementing the isInDb promise and followed it with creating a new user, not optimal solution

--- routes/users.js
+++ routes/users.js
@@ -11,21 +11,24 @@ module.exports = (knex) => {
 
 
   router.get('/new_user', (req, res) => {
-    // we will need to see if the new user is already in the db, so if the username matches someone else
+
+// crafting the user-object
     const userObj = {
-      username: req.body.username || 1,
+      username: req.body.username || '@PxG',
       first_name: req.body.first_name || 'pat',
       last_name: req.body.first_name || 'dnjewd',
       email: req.body.email || "123@123",
       password: req.body.password || '1234'
      }
 
+     // the first promise gets called to check if the username is in the databse
      db_helper.isUsernameInUsers('@PxG')
-     .then((result) => {res.send("Username Exists, please pick another one!" )})
-     .catch((error) => {console.log('it is in!'); res.send('il nexiste pas')});
-    // .db_helper
-    // .newDbInput('users', userObj)
-    // .then(res.send('Successfully Written!'))
+     .then((result) => {return res.send("Username Exists, please pick another one!" )})
+     .catch( // new promise
+            db_helper
+            .newDbInput('users', userObj)
+            .then(() => {return res.send('Successfully Written!')}) // the second promise is called when there is an error (saying there is someone in the db)
+);
 
   });
 
