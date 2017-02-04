183ba58c20128377e49266f35acc0f7cfa850893
Dev43 <patrick.guay43@gmail.com>
2017-02-04 11:17:27 -0500

    changed the goodreads API to google books api

--- public/scripts/ProductsAndBooksApi.js
+++ public/scripts/ProductsAndBooksApi.js
@@ -1,58 +1,9 @@
 
-
-// GOODREADS API
-// key: MuedSVaIqiN37Elu0LWQOQ
-// secret: EgSWTEDpA52laYlPlCMW8hx00t8Vyi8xPGOIbrOQVk
-
-// Changes XML to JSON
-function xmlToJson(xml) {
-
-  // Create the return object
-  var obj = {};
-
-  if (xml.nodeType == 1) { // element
-    // do attributes
-    if (xml.attributes.length > 0) {
-    obj["@attributes"] = {};
-      for (var j = 0; j < xml.attributes.length; j++) {
-        var attribute = xml.attributes.item(j);
-        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-      }
-    }
-  } else if (xml.nodeType == 3) { // text
-    obj = xml.nodeValue;
-  }
-
-  // do children
-  if (xml.hasChildNodes()) {
-    for(var i = 0; i < xml.childNodes.length; i++) {
-      var item = xml.childNodes.item(i);
-      var nodeName = item.nodeName;
-      if (typeof(obj[nodeName]) == "undefined") {
-        obj[nodeName] = xmlToJson(item);
-      } else {
-        if (typeof(obj[nodeName].push) == "undefined") {
-          var old = obj[nodeName];
-          obj[nodeName] = [];
-          obj[nodeName].push(old);
-        }
-        obj[nodeName].push(xmlToJson(item));
-      }
-    }
-  }
-  return obj;
-};
+// using GOOGLE BOOKS API instead
 
 
-var theURLToFOllow = "https://www.goodreads.com/search.xml?key=YOUR_KEY&q=Ender%27s+Game"
-
-
-let apiKey =  "?key=MuedSVaIqiN37Elu0LWQOQ";
-let url = "https://www.goodreads.com/search.xml";
-
-let query = "Ender's Game";
-
-
+let url = "https://www.googleapis.com/books/v1/volumes?";
+let query = "read the sun also rises"
 
 function getBookName(string){
   string =  string.split(" ");
@@ -61,7 +12,7 @@ function getBookName(string){
 }
 
 function makeQuery(query){
-  return url + apiKey + "&q=" + encodeURI(query);
+  return url + "q=" + encodeURI(query);
 }
 
 
@@ -72,11 +23,10 @@ function createSettings(query, id){
   return {
     "async": true,
     "crossDomain": true,
-    "url":  'https://cors-anywhere.herokuapp.com/' + makeQuery(query),
+    "url":   makeQuery(query),
     "method": "GET",
-    "headers": {},
     "data": "{}",
-    // "dataType":'jsonp'
+    "dataType":'jsonp'
   }
 }
 
@@ -86,7 +36,76 @@ function createSettings(query, id){
 $.ajax(createSettings(query)).done(function (books) {
 
     console.log(books)
-   console.log(xmlToJson(books));
+
+    let bookInfo = {
+      title: books.items[0].volumeInfo.title,
+      authorsArray: books.items[0].volumeInfo.authors,
+      publisher: books.items[0].volumeInfo.publisher,
+      description: books.items[0].volumeInfo.description,
+      categoriesArray: books.items[0].volumeInfo.categories,
+      rating: books.items[0].volumeInfo.averageRating,
+      previewLink: books.items[0].volumeInfo.previewLink,
+      rating: books.items[0].volumeInfo.averageRating,
+      smallImage: books.items[0].volumeInfo.imageLinks.smallThumbnail
+    }
+
+    console.log(bookInfo)
 
   })
+
+
+
+
+// // GOODREADS API
+// // key: MuedSVaIqiN37Elu0LWQOQ
+// // secret: EgSWTEDpA52laYlPlCMW8hx00t8Vyi8xPGOIbrOQVk
+
+// // Changes XML to JSON
+// function xmlToJson(xml) {
+
+//   // Create the return object
+//   var obj = {};
+
+//   if (xml.nodeType == 1) { // element
+//     // do attributes
+//     if (xml.attributes.length > 0) {
+//     obj["@attributes"] = {};
+//       for (var j = 0; j < xml.attributes.length; j++) {
+//         var attribute = xml.attributes.item(j);
+//         obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+//       }
+//     }
+//   } else if (xml.nodeType == 3) { // text
+//     obj = xml.nodeValue;
+//   }
+
+//   // do children
+//   if (xml.hasChildNodes()) {
+//     for(var i = 0; i < xml.childNodes.length; i++) {
+//       var item = xml.childNodes.item(i);
+//       var nodeName = item.nodeName;
+//       if (typeof(obj[nodeName]) == "undefined") {
+//         obj[nodeName] = xmlToJson(item);
+//       } else {
+//         if (typeof(obj[nodeName].push) == "undefined") {
+//           var old = obj[nodeName];
+//           obj[nodeName] = [];
+//           obj[nodeName].push(old);
+//         }
+//         obj[nodeName].push(xmlToJson(item));
+//       }
+//     }
+//   }
+//   return obj;
+// };
+
+
+// var theURLToFOllow = "https://www.goodreads.com/search.xml?key=YOUR_KEY&q=Ender%27s+Game"
+
+// 'https://cors-anywhere.herokuapp.com/' +
+// let apiKey =  "?key=MuedSVaIqiN37Elu0LWQOQ";
+// let url = "https://www.goodreads.com/search.xml";
+
+// let query = "Ender's Game";
+
 
