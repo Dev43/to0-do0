a46d1096671687f2313dad735a9a21818045feb3
Dev43 <patrick.guay43@gmail.com>
2017-02-04 15:19:15 -0500

    implemented yelp api searching for a specifi restaurant

--- public/scripts/restaurantReviewsAPI.js
+++ public/scripts/restaurantReviewsAPI.js
@@ -1,14 +1,20 @@
-// assuming you are in montreal
+// // assuming you are in montreal
+// // probably need to allow for location in the app already
 
-// navigator.geolocation.getCurrentPosition(function(position) {
-//   console.log(position.coords.latitude, position.coords.longitude);
-// });
+// // navigator.geolocation.getCurrentPosition(function(position) {
+// //   console.log(position.coords.latitude, position.coords.longitude);
+// // });
 
-//AUTHENTICATION // will need to hide all of these!
+// //AUTHENTICATION // will need to hide all of these!
+let query = "try shwartz";
+let searchUrl = "https://api.yelp.com/v3/businesses/search?";
+let businessUrl = "https://api.yelp.com/v3/businesses/"
+let search = "term=";
+
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": 'https://cors-anywhere.herokuapp.com/' + "https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972",
+  "url": 'https://cors-anywhere.herokuapp.com/' + makeQuery(query),
   "method": "GET",
   "headers": {
     "authorization": "Bearer o9NWDKOyt5OThXDjNeFb_1HNfrhxHycMgYuePjJUe66Bk05Zfy2m5A4_d9raPBh-Wzc-cyy60fe-80Gjk1hxl1dHp3wo8IuKtt9XNW8CyG9wQDDuN-PcbjaHdA2WWHYx",
@@ -17,57 +23,73 @@ var settings = {
 
 }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+
+function createSettings(url, query, method){
+  return {
+    "async": true,
+    "crossDomain": true,
+    "url": 'https://cors-anywhere.herokuapp.com/' + makeQuery(url, query, method),
+    "method": "GET",
+    "headers": {
+    "authorization": "Bearer o9NWDKOyt5OThXDjNeFb_1HNfrhxHycMgYuePjJUe66Bk05Zfy2m5A4_d9raPBh-Wzc-cyy60fe-80Gjk1hxl1dHp3wo8IuKtt9XNW8CyG9wQDDuN-PcbjaHdA2WWHYx",
+    "cache-control": "no-cache",
+    }
+  }
+}
+
+// let query = "read the sun also rises"
+
+
+function getRestaurantName(string){
+  string =  string.split(" ");
+  string.shift();
+ return string.join(" ");
+}
+
+function makeQuery(url, query, method){
+  if(!method){
+    return url + query;
+  } else {
+  return url + method + encodeURI(query) + "&location=montreal";
+  }
+}
+
+
+$.ajax(createSettings(searchUrl, getRestaurantName(query), search )).done(function (restaurants) {
+
+  let relevantRestaurantId = restaurants.businesses[0].id;
+  console.log(relevantRestaurantId)
+
+  $.ajax(createSettings(businessUrl, relevantRestaurantId, "")).done(function (resto) {
+    console.log(resto);
+
+
+  });
+
+
+
 });
-// let url = "https://api.yelp.com/v3/businesses/search";
-// // let query = "read the sun also rises"
-// let query = "try l'gros luxe"
-
-// function getRestaurantName(string){
-//   string =  string.split(" ");
-//   string.shift();
-//  return string.join(" ");
-// }
-
-// function makeQuery(query){
-//   return url + "q=" + encodeURI(query);
-// }
 
 
 
 
-// function createSettings(query, id){
-//   console.log(makeQuery(query))
-//   return {
-//     "async": true,
-//     "crossDomain": true,
-//     "url":   makeQuery(query),
-//     "method": "GET",
-//     "data": "{}",
-//     "dataType":'jsonp'
-//   }
-// }
 
+// // $.ajax(createSettings(query)).done(function (restaurants) {
 
+// //     console.log(restaurants)
 
+// //     // let bookInfo = {
+// //     //   title: books.items[0].volumeInfo.title,
+// //     //   authorsArray: books.items[0].volumeInfo.authors,
+// //     //   publisher: books.items[0].volumeInfo.publisher,
+// //     //   description: books.items[0].volumeInfo.description,
+// //     //   categoriesArray: books.items[0].volumeInfo.categories,
+// //     //   rating: books.items[0].volumeInfo.averageRating,
+// //     //   previewLink: books.items[0].volumeInfo.previewLink,
+// //     //   rating: books.items[0].volumeInfo.averageRating,
+// //     //   smallImage: books.items[0].volumeInfo.imageLinks.smallThumbnail
+// //     // }
 
-// $.ajax(createSettings(query)).done(function (restaurants) {
+// //     // console.log(bookInfo)
 
-//     console.log(restaurants)
-
-//     // let bookInfo = {
-//     //   title: books.items[0].volumeInfo.title,
-//     //   authorsArray: books.items[0].volumeInfo.authors,
-//     //   publisher: books.items[0].volumeInfo.publisher,
-//     //   description: books.items[0].volumeInfo.description,
-//     //   categoriesArray: books.items[0].volumeInfo.categories,
-//     //   rating: books.items[0].volumeInfo.averageRating,
-//     //   previewLink: books.items[0].volumeInfo.previewLink,
-//     //   rating: books.items[0].volumeInfo.averageRating,
-//     //   smallImage: books.items[0].volumeInfo.imageLinks.smallThumbnail
-//     // }
-
-//     // console.log(bookInfo)
-
-//   })
+// //   })
