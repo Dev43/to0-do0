722e961be618977271d0e6ca6838eb665a2214e9
Dev43 <patrick.guay43@gmail.com>
2017-02-05 11:55:48 -0500

    added getMovie function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -268,10 +268,45 @@ function getBook(query){
 
 }
 
-getBook("read lord of the rings")
 
+function getMovie(query){
 
+  let apiKey =  "**API-KEY**";
+  let url = "https://api.themoviedb.org/3/";
+  let search = 'search/multi';
+  let movie = 'movie/';
+  let language = '&language=en-US';
+  let theQuery = getMovieName(query)
 
+  function getMovieName(string){
+    string =  string.split(" ");
+    string.shift();
+   return string.join(" ");
+  }
+
+  function makeQuery(method, query, id){
+    return url + method + id + apiKey + "&query=" + encodeURI(query);
+  }
+
+  function createSettings(method, query, id){
+    return {
+      "async": true,
+      "crossDomain": true,
+      "url": makeQuery(method, query, id),
+      "method": "GET"
+    }
+  }
+
+    $.ajax(createSettings(search, theQuery, "")).done(function (moviesOrTvShows) {
+      let movieId = moviesOrTvShows.results[0].id;
+      // movies.media to know if tv show or movie -- implement later
+      $.ajax(createSettings(movie, "",  movieId)).done(function(movie){
+        let movieInfo = standardInformationBuilder(movie.overview, "", movie.vote_average, movie.title );
+        return movieInfo;
+      })
+
+    });
+  }
 });
 
 
