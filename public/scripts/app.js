98c21cbf6733fae19256a84298b4aeea8711ee0b
Dev43 <patrick.guay43@gmail.com>
2017-02-05 12:01:22 -0500

    add getResto function to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -307,6 +307,61 @@ function getMovie(query){
 
     });
   }
+
+function getRestaurant(query){
+  let searchUrl = "https://api.yelp.com/v3/businesses/search?";
+  let businessUrl = "https://api.yelp.com/v3/businesses/"
+  let search = "term=";
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": 'https://cors-anywhere.herokuapp.com/' + makeQuery(query),
+    "method": "GET",
+    "headers": {
+      "authorization": "**AUTHORIZATION**",
+      "cache-control": "no-cache",
+    }
+  }
+
+  function createSettings(url, query, method){
+    return {
+      "async": true,
+      "crossDomain": true,
+      "url": 'https://cors-anywhere.herokuapp.com/' + makeQuery(url, query, method),
+      "method": "GET",
+      "headers": {
+      "authorization": "**AUTHORIZATION**",
+      "cache-control": "no-cache",
+      }
+    }
+  }
+
+  function getRestaurantName(string){
+    string =  string.split(" ");
+    string.shift();
+   return string.join(" ");
+  }
+
+  function makeQuery(url, query, method){
+    if(!method){
+      return url + query;
+    } else {
+    return url + method + encodeURI(query) + "&location=montreal";
+    }
+  }
+
+  $.ajax(createSettings(searchUrl, getRestaurantName(query), search )).done(function (restaurants) {
+
+    let relevantRestaurantId = restaurants.businesses[0].id;
+    $.ajax(createSettings(businessUrl, relevantRestaurantId, "")).done(function (resto) {
+      let restoInfo = standardInformationBuilder(resto.phone, resto.image_url, resto.rating, resto.name)
+      return resto;
+    });
+  });
+  }
+
+
+
 });
 
 
